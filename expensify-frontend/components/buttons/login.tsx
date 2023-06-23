b2e42a99cc37207ddc96adbb333ec3d8d8a26c2f
Nhan0504 <ttn0504ltt@gmail.com>
2023-06-23 20:45:56 +0700

    Move styling to css file

--- expensify-frontend/components/buttons/login.tsx
+++ expensify-frontend/components/buttons/login.tsx
@@ -1,21 +1,20 @@
 "use client";
 
 import { useState } from "react";
-import "./style.css"
+import "./style.css";
 
 export function LoginButton() {
-    const [login, setLogin] = useState(false);
+  const [login, setLogin] = useState(false);
 
-    const handleLogin = () => {
-        setLogin(true);
-      };
+  const handleLogin = () => {
+    setLogin(true);
+  };
 
-    return(
-        <div>
-            <button onClick={handleLogin} className="styled-button">
+  return (
+    <div>
+      <button onClick={handleLogin} className="styled-button">
         Log in
       </button>
-      <p>{login ? "Logged in successfully" : ""}</p>
-        </div>
-    )
+    </div>
+  );
 }

fde7cb683e96e4f16fd6d5b18e9a373898f1c77e
Nhan0504 <ttn0504ltt@gmail.com>
2023-06-24 21:11:58 +0700

    Send POST request to the login endpoint

--- expensify-frontend/components/form/login.tsx
+++ expensify-frontend/components/form/login.tsx
@@ -1,17 +1,70 @@
 "user-client";
 
+import { useState } from "react";
 import { LoginButton } from "../buttons/login";
 import { FormInput } from "../input/formInput";
+import { redirect } from "next/navigation";
+//import { onLogin } from "@/app/api/login";
 import "./style.css";
 
 export function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPasword] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const handleUsername = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPasword(event.target.value);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = {
+      username: username,
+      password: password,
+    };
+    console.log(data);
+
+    const url = "http://localhost:8080/login";
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams(data),
+    })
+      .then((res) =>
+        res.ok
+          ? (window.location.href = "http://localhost:3000")
+          : setErrorMsg("Wrong")
+      )
+      .catch((e) => console.log(e));
+  };
+
   return (
     <div className="backdrop-blur-sm bg-black/30 styled-login-form rounded-3xl">
-      <form>
-        <FormInput type={"text"} text={"Username"} />
-        <FormInput type={"password"} text={"Password"} />
+      <form onSubmit={handleSubmit} method="post">
+        <FormInput
+          type={"text"}
+          text={"Username"}
+          input={username}
+          onInput={handleUsername}
+        />
+        <FormInput
+          type={"password"}
+          text={"Password"}
+          input={password}
+          onInput={handlePassword}
+        />
+        <div style={{position: "absolute", paddingLeft: 130, paddingBottom: 20}}>
+          <p>{errorMsg}</p>
+        </div>
+
+        <LoginButton />
       </form>
-      <LoginButton />
     </div>
   );
 }

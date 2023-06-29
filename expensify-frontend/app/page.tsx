aa51552e1cdc5806043b995e1dc7fb275220b4ab
Nhan0504 <ttn0504ltt@gmail.com>
2023-06-29 12:00:26 +0700

    Redirect user to login page

--- expensify-frontend/app/page.tsx
+++ expensify-frontend/app/page.tsx
@@ -1,19 +1,21 @@
 "use client";
 
 import { useUser } from "../context/UserContext";
+import { useRouter } from 'next/navigation';
 import "./style.css";
 
 export default function Home() {
   const { user } = useUser();
+  const router = useRouter();
+
+  if (!user) {
+    router.push("login");
+  }
 
   return (
     <main className="background-home">
       <div className="welcome-container">
-        {user ? (
-          <p className="animate-bounce welcome">Hello, {user.username}!</p>
-        ) : (
-          <p>Please log in</p>
-        )}
+        {user && <p className="animate-bounce welcome">Hello, {user.username}!</p> }
       </div>
     </main>
   );
